71e9f0c30de9f9916495c92d0b85e83ac93dad9e
dudrovaMaria01 <dudrovp@inbox.ru>
2021-09-06 11:34:32 +0000

    work in the lesson 11

---
+++ proba.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.19  Distrib 10.3.29-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: db1
+-- ------------------------------------------------------
+-- Server version	10.3.29-MariaDB-0+deb10u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `proba`
+--
+
+DROP TABLE IF EXISTS `proba`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proba` (
+  `id` int(35) NOT NULL AUTO_INCREMENT,
+  `name` char(255) DEFAULT NULL,
+  `data_privosa` date DEFAULT NULL,
+  `srok` int(255) DEFAULT NULL,
+  `Kolichestvo` int(255) DEFAULT NULL,
+  `Cost` float(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proba`
+--
+
+LOCK TABLES `proba` WRITE;
+/*!40000 ALTER TABLE `proba` DISABLE KEYS */;
+INSERT INTO `proba` VALUES (1,'kolbasa','2005-12-06',5,200,350.00),(2,'milk','2005-12-05',5,200,35.00),(3,'water','2005-12-04',5,100,35.00),(4,'bread','2005-12-03',5,150,35.00),(5,'tomat','2005-12-02',5,200,90.00),(6,'potato','2005-12-01',5,100,30.00),(7,'cucumber','2005-12-31',5,150,100.00),(8,'cheese','2005-12-30',5,200,200.00),(9,'cake','2005-12-29',5,100,400.00),(10,'sweets','2005-12-28',5,150,150.00),(11,'cooky','2005-12-27',5,200,230.00),(12,'masik','2005-12-26',5,100,70.00),(13,'fish','2005-12-25',5,150,350.00),(14,'meat','2005-12-24',5,200,350.00),(15,'ice cream','2005-12-23',5,100,30.00);
+/*!40000 ALTER TABLE `proba` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-06 11:25:49
